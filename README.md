da0d7f2629e2891a7450c27bb051322d4d411bc3
parabola2004 <chengzhuo2004@qq.com>
2026-02-28 18:01:13 +0800

    more text operations

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Spargel LM
 
+![GitHub top language](https://img.shields.io/github/languages/top/SpargelEngine/lm)
+![GitHub License](https://img.shields.io/github/license/SpargelEngine/lm)
+![GitHub commit activity](https://img.shields.io/github/commit-activity/t/SpargelEngine/lm)
+![GitHub last commit](https://img.shields.io/github/last-commit/SpargelEngine/lm)
+
 This is a project for learning and testing language models.
--- src/spargel_lm/text_source.py
+++ src/spargel_lm/text_source.py
@@ -1,9 +1,13 @@
 import gzip
+import json
 import logging
+import os
 from pathlib import Path
 from typing import Any, Iterator, Literal, override
 
+import regex
 from pydantic import BaseModel, Field
+from regex import Pattern
 
 from .typing import StrOrPath
 
@@ -11,7 +15,8 @@ logger = logging.getLogger(__name__)
 
 
 def _resolve_parent(path: StrOrPath):
-    return Path(path).resolve().parent
+    p = Path(path).resolve()
+    return p.parent if p.is_file() else p
 
 
 #### Text sources ####
@@ -20,7 +25,50 @@ def _resolve_parent(path: StrOrPath):
 class TextSourceModel(BaseModel):
     """Base class for text source models"""
 
+    comment: str = ""
+
     def get_texts(self, this_path: StrOrPath) -> Iterator[str]: ...
+
+
+class FindFileSource(TextSourceModel):
+    """Find files in a directory."""
+
+    type: Literal["find"]
+    base: str = "."
+    paths: list[str] = ["."]
+    file_pattern: str | None = None
+    dir_pattern: str | None = None
+
+    @override
+    def get_texts(self, this_path):
+        dir_pattern = regex.compile(self.dir_pattern) if self.dir_pattern else None
+        file_pattern = regex.compile(self.file_pattern) if self.file_pattern else None
+
+        base_path = _resolve_parent(this_path) / self.base
+
+        for path in self.paths:
+            yield from (
+                str(p)
+                for p in self._search_dir(
+                    base_path / path, file_pattern=file_pattern, dir_pattern=dir_pattern
+                )
+            )
+
+    def _search_dir(
+        self,
+        dir: StrOrPath,
+        *,
+        file_pattern: Pattern[str] | None = None,
+        dir_pattern: Pattern[str] | None = None,
+    ) -> Iterator[Path]:
+        for cur_dir, dirs, files in os.walk(dir):
+            if dir_pattern is not None:
+                dirs[:] = [d for d in dirs if dir_pattern.fullmatch(d)]
+
+            for file in files:
+                if file_pattern is not None and not file_pattern.fullmatch(file):
+                    continue
+                yield Path(cur_dir) / file
 
 
 class PlainTextSource(TextSourceModel):
@@ -44,17 +92,18 @@ class ProcessSource(TextSourceModel):
     @override
     def get_texts(self, this_path):
         for source in self.sources:
-            texts = list(source.get_texts(this_path))
-            for operation in self.operations:
-                tmp: list[str] = []
-                for text in texts:
-                    tmp.extend(operation.process(text, this_path))
-                texts = tmp
+            for text in source.get_texts(this_path):
+                texts = [text]
+                for operation in self.operations:
+                    tmp: list[str] = []
+                    for text in texts:
+                        tmp.extend(operation.process(text, this_path))
+                    texts = tmp
 
-            yield from texts
+                yield from texts
 
 
-type TextSource = PlainTextSource | ProcessSource
+type TextSource = FindFileSource | PlainTextSource | ProcessSource
 
 
 class TextSourceWrapperModel(BaseModel):
@@ -66,6 +115,8 @@ class TextSourceWrapperModel(BaseModel):
 
 class OperationModel(BaseModel):
     """Base class for text operation models"""
+
+    comment: str = ""
 
     def process(self, text: str, this_path: StrOrPath) -> Iterator[str]: ...
 
@@ -95,15 +146,84 @@ class ReadFileOperation(OperationModel):
             logger.warning(f'UnicodeDecodeError in file: "{path}"')
 
 
+class ReferenceOperation(OperationModel):
+    """Reference external operations."""
+
+    type: Literal["ref"]
+    base: str = "."
+    paths: list[str]
+
+    @override
+    def process(self, text: str, this_path):
+        texts = [text]
+        for path in self.paths:
+            ref_path = _resolve_parent(this_path) / self.base / path
+            for operation in self._get_operations(ref_path):
+                tmp: list[str] = []
+                for text in texts:
+                    tmp.extend(operation.process(text, ref_path))
+                texts = tmp
+
+        yield from texts
+
+    def _get_operations(self, path: StrOrPath) -> Iterator[Operation]:
+        with open(path, "r") as f:
+            array = json.load(f)
+
+        for obj in array:
+            yield OperationWrapperModel.model_validate({"operation": obj}).operation
+
+
+class ReplaceOperation(OperationModel):
+    """Replace All"""
+
+    type: Literal["replace"]
+    regex: bool = False
+    old: str
+    new: str
+    repeat: bool = False
+    per_line: bool = False
+
+    @override
+    def process(self, text: str, this_path):
+        if self.per_line:
+            yield "\n".join(self._apply_replace(line) for line in text.splitlines())
+        else:
+            yield self._apply_replace(text)
+
+    def _apply_replace(self, text: str) -> str:
+        if self.repeat:
+            last_text = text
+            while True:
+                if self.regex:
+                    text = regex.sub(self.old, self.new, text)
+                else:
+                    text = text.replace(self.old, self.new)
+
+                if text == last_text:
+                    break
+                last_text = text
+            return text
+        else:
+            if self.regex:
+                return regex.sub(self.old, self.new, text)
+            else:
+                return text.replace(self.old, self.new)
+
+
 class RightStripOperation(OperationModel):
     """Right-strip text (remove trailing whitespace)."""
 
     type: Literal["rstrip"]
     chars: str | None = None
+    per_line: bool = False
 
     @override
     def process(self, text: str, this_path):
-        yield text.rstrip(self.chars)
+        if self.per_line:
+            yield "\n".join(line.rstrip(self.chars) for line in text.splitlines())
+        else:
+            yield text.rstrip(self.chars)
 
 
 class SplitLinesOperation(OperationModel):
@@ -122,13 +242,17 @@ class StripOperation(OperationModel):
 
     type: Literal["strip"]
     chars: str | None = None
+    per_line: bool = False
 
     @override
     def process(self, text: str, this_path):
-        yield text.strip(self.chars)
+        if self.per_line:
+            yield "\n".join(line.strip(self.chars) for line in text.splitlines())
+        else:
+            yield text.strip(self.chars)
 
 
-type Operation = ReadFileOperation | RightStripOperation | SplitLinesOperation | StripOperation
+type Operation = ReadFileOperation | ReferenceOperation | ReplaceOperation | RightStripOperation | SplitLinesOperation | StripOperation
 
 
 class OperationWrapperModel(BaseModel):
