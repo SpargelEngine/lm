cdd83f9b86149264ed1e5b3ded29d6dbd7ed7241
Tianjiao Nie <ivwumupy@gmail.com>
2026-03-02 11:19:11 +0800

    layer: linear

---
+++ tests/layer/test_linear.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn.functional as F
+
+from spargel_lm.layer.linear import Linear
+
+
+def test_linear_shap():
+    layer = Linear(input_dimension=3, output_dimension=4)
+
+    x = torch.randn(2, 5, 3)
+    y = layer(x)
+
+    assert y.shape == (2, 5, 4)
